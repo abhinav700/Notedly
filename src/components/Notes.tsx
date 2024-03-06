c8e0ffea331262567f69739430626e67768509fc
abhinav agarwal <abhinavagrawal700@gmail.com>
2024-03-06 10:29:45 +0530

    Added key prop

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -29,7 +29,7 @@ function Notes() {
         <div className="flex flex-wrap space-x-3">
         {notes
           ? notes.map((item: any) => {
-              return <Noteitem  note={item} />;
+              return <Noteitem key = {item._id} note={item} />;
             })
           : null}
       </div>
