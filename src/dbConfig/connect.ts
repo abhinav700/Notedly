9d9f272bc23f12e0a6963c5ad0f6abb3ea75b499
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-09-22 10:26:47 +0530

    add setInterval to prevent inactivity on cluster

--- src/dbConfig/connect.ts
+++ src/dbConfig/connect.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 export let connect = async () => {
   try {
-    mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URI!);
+    setInterval(() => {
+      mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URI!);
+    }, 5 * 60 * 60 * 1000);
+
     let connection = mongoose.connection;
     connection.on("connected", () => {
       console.log("Connected Mongo Db successfuly");
