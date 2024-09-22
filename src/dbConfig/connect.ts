d95ce9389d9f18fa727841f8608dc7125230b020
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-09-22 10:44:29 +0530

    reverting the changes

--- src/dbConfig/connect.ts
+++ src/dbConfig/connect.ts
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 export let connect = async () => {
   try {
-    setInterval(() => {
-      mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URI!);
-    }, 5 * 60 * 60 * 1000);
+    mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URI!);
 
     let connection = mongoose.connection;
     connection.on("connected", () => {
